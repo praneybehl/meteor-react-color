a0985d9d0804d2746bb0592e57223190aeba4715
Praney Behl <praneybehl@gmail.com>
2015-11-06 23:55:45 +1100

    package dependencies updated

--- package.js
+++ package.js
@@ -1,24 +1,21 @@
 Package.describe({
   name: 'praneybehl:react-color',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  version: '1.2.1',
+  summary: 'React Color Pickers with 7 themes - Sketch, Chrome, Photoshop, Slider, Compact, Material, Swatches packaged for Meteor',
+  git: 'https://github.com/praneybehl/meteor-react-color',
   documentation: 'README.md'
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
-  api.use('ecmascript');
-  api.addFiles('react-color.js');
+api.use('ecmascript');
+api.use('react@0.1.13');
+api.use('cosmos:browserify@0.8.3', 'client');
+api.addFiles('package.browserify.js', 'client');
+api.addFiles('package.browserify.options.json', 'client');
+api.export('ColorPicker');
 });
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('praneybehl:react-color');
-  api.addFiles('react-color-tests.js');
+
+Npm.depends({
+"react-color": "1.2.1",
+"exposify": "0.5.0"
 });
