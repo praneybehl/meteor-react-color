bf204fb94148215b5ab6627214d25dd98417015e
Praney Behl <praneybehl@gmail.com>
2015-11-07 00:12:37 +1100

    fixed meteor version constraint

--- package.js
+++ package.js
@@ -7,6 +7,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
+    api.versionsFrom('1.2.1');
     api.use('ecmascript');
     api.use('react@0.1.13');
     api.use('cosmos:browserify@0.8.3', 'client');
